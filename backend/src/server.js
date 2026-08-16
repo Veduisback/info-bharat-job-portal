8ecf479b3c8449906702c6a2424a607e36226503
Vedang Jaiswal <veduisback@gmail.com>
2026-08-16 23:21:29 +0530

    Set up Express backend and MongoDB

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Info Bharat Job Portal API is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
